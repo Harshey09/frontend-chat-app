b4811c27d593d6183b4d20b22b15e04aa512435d
Harshey09 <111156962+Harshey09@users.noreply.github.com>
2024-06-20 01:19:37 +0530

    Updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+   plugins: [react()],
   server: {
-    port:process.env.PORT || 3000,
+  host: true,
+  strictPort: true,
+  port: 8000,
   }
-})
+});
 
