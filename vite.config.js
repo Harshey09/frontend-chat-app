e2b049c7c17f818e46bc07f68341f389745134bf
HARSHIT <thakurharshitsingh10@gmail.com>
2024-06-20 01:15:03 +0530

    PORT

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port:process.env.PORT || 5173,
-  }
-})
-
+server: {
+host: true,
+strictPort: true,
+port: 8000,
+}
+});
